f866384974dd449ae9103ae938a4092e4e589a17
kuyoru <kuyoru730@gmail.com>
2023-10-12 18:13:51 +0800

    c# version designing

---
+++ csapp/ktop/classes/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktop.classes
+{
+    public class Config
+    {
+        public double width { set; get; }
+        public double height { set; get; }
+    }
+}
---
+++ csapp/ktop/state/Store.cs
@@ -0,0 +1,26 @@
+﻿using ktop.classes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ktop.state
+{
+    public static class Store
+    {
+        public static string configPath = "./resources/config/default.json";
+        public static Config? __config__;
+
+        public static void ReadConfig()
+        {
+            var jsonStr = File.ReadAllText(configPath);
+            __config__ = JsonConvert.DeserializeObject<Config>(jsonStr);
+        }
+    }
+}
