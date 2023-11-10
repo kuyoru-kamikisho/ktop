aaabe7720e73018369819c7a5409320e4a1bd5e1
kuyoru <kuyoru730@gmail.com>
2023-11-10 16:24:09 +0800

    search extend from kamikisho

--- csapp/layout2/KPages/KSitesPage.xaml.cs
+++ csapp/layout2/KPages/KSitesPage.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +19,22 @@ using System.Windows.Shapes;
 
 namespace ktop2.KPages
 {
-    /// <summary>
-    /// KSitesPage.xaml 的交互逻辑
-    /// </summary>
     public partial class KSitesPage : Page
     {
+
         public KSitesPage()
         {
             InitializeComponent();
+            ReadFiles();
+        }
+        public void ReadFiles()
+        {
+            var search = File.ReadAllText("./KResources/search.json");
+            var webs = File.ReadAllText("./KResources/webs.json");
+            var searcherList = JsonConvert.DeserializeObject<List<SitePageO>>(search);
+            var webList = JsonConvert.DeserializeObject<List<SitePageO>>(webs);
+            KStore.SST.SearchMode = searcherList[KStore.SST.SearchIndex];
+            //searchmode.DataContext = KStore.SST.SearchMode;
         }
     }
 }
